bb4a6fba622641bf4233e9769decd24cf9bde527
d0422 <rlfehd2013@naver.com>
2025-01-27 14:28:51 +0900

    feat: 기본 홈화면 액티비티 페이지 추가

---
+++ client/src/activities/Home.tsx
@@ -0,0 +1,13 @@
+import { AppScreen } from '@stackflow/plugin-basic-ui';
+
+export default function Home() {
+  return (
+    <AppScreen
+      appBar={{
+        title: '커뮤니티',
+      }}
+    >
+      <div>홈화면</div>
+    </AppScreen>
+  );
+}
--- client/src/stackflow.ts
+++ client/src/stackflow.ts
@@ -1,14 +1,18 @@
 import { stackflow } from '@stackflow/react';
 import { basicRendererPlugin } from '@stackflow/plugin-renderer-basic';
 import { basicUIPlugin } from '@stackflow/plugin-basic-ui';
+import Home from './activities/Home';
 
 export const { Stack, useFlow } = stackflow({
   transitionDuration: 350,
-  activities: {},
+  activities: {
+    Home,
+  },
   plugins: [
     basicRendererPlugin(),
     basicUIPlugin({
       theme: 'cupertino',
     }),
   ],
+  initialActivity: () => 'Home',
 });
