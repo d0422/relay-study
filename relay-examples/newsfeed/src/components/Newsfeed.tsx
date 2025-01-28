f210d5d93da39cd11cecb81d83e108349f44fd93
d0422 <rlfehd2013@naver.com>
2025-01-28 14:17:01 +0900

    feat: paginationFragment 적용

--- relay-examples/newsfeed/src/components/Newsfeed.tsx
+++ relay-examples/newsfeed/src/components/Newsfeed.tsx
@@ -1,27 +1,60 @@
 import * as React from "react";
 import Story from "./Story";
 import { graphql } from "relay-runtime";
-import { useLazyLoadQuery } from "react-relay";
+import { useLazyLoadQuery, usePaginationFragment } from "react-relay";
 import type { NewsfeedQuery as NewsfeedQueryType } from "./__generated__/NewsfeedQuery.graphql";
-
+import InfiniteScrollTrigger from "./InfiniteScrollTrigger";
+import { NewsfeedContentsFragment$key } from "./__generated__/NewsfeedContentsFragment.graphql";
 const NewsfeedQuery = graphql`
   query NewsfeedQuery {
-    topStories {
-      id
-      ...StoryFragment
+    ...NewsfeedContentsFragment
+  }
+`;
+
+const NewsfeedContentsFragment = graphql`
+  fragment NewsfeedContentsFragment on Query
+  @refetchable(queryName: "NewsfeedContentsRefetchQuery")
+  @argumentDefinitions(
+    cursor: { type: "String" }
+    count: { type: "Int", defaultValue: 3 }
+  ) {
+    viewer {
+      newsfeedStories(after: $cursor, first: $count)
+        @connection(key: "NewsfeedContentsFragment_newsfeedStories") {
+        edges {
+          node {
+            id
+            ...StoryFragment
+          }
+        }
+      }
     }
   }
 `;
 
 export default function Newsfeed() {
-  const data = useLazyLoadQuery<NewsfeedQueryType>(NewsfeedQuery, {});
-  const { topStories } = data;
+  const queryData = useLazyLoadQuery<NewsfeedQueryType>(NewsfeedQuery, {});
+  const { data, loadNext, hasNext, isLoadingNext } = usePaginationFragment<
+    NewsfeedQueryType,
+    NewsfeedContentsFragment$key
+  >(NewsfeedContentsFragment, queryData);
+
+  const storyEdges = data.viewer.newsfeedStories.edges;
+
+  function onEndReached() {
+    loadNext(1);
+  }
 
   return (
     <div className="newsfeed">
-      {topStories.map((story) => (
-        <Story story={story} key={story.id} />
+      {storyEdges.map((story) => (
+        <Story story={story.node} key={story.node.id} />
       ))}
+      <InfiniteScrollTrigger
+        onEndReached={onEndReached}
+        hasNext={hasNext}
+        isLoadingNext={isLoadingNext}
+      />
     </div>
   );
 }
