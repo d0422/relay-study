0dc33bd954be7270b13c7fb0e51b78fffe9e436a
d0422 <rlfehd2013@naver.com>
2025-01-30 01:03:48 +0900

    chore: 기본 유저 및 기본 글 추가

--- server/model/MockDatabase.js
+++ server/model/MockDatabase.js
@@ -1,6 +1,16 @@
+const defaultUser = { id: 0, name: 'Gildong' };
 const MockDatabase = {
-  users: [],
-  articles: [],
+  users: [defaultUser],
+  articles: [
+    {
+      id: 0,
+      title: '글을 써보았어요',
+      content: 'GraphQL+Relay를 처음 써보아요. 좋은 것 같아요~',
+      author: defaultUser,
+      likeNum: 0,
+      isLiked: false,
+    },
+  ],
 };
 
 module.exports = MockDatabase;
